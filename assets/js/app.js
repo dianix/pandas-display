fc341f7073c6e3c414ef19e58e33da3b247fc749
Diana Vázquez <dianavazquez.dcg@gmail.com>
2017-04-18 01:00:57 -0500

    agregadas funciones para ocultar parrafos

---
+++ assets/js/app.js
@@ -0,0 +1,37 @@
+
+var origen = document.getElementById("boton-origen");
+var extincion = document.getElementById("boton-extincion");
+var restaurar = document.getElementById("boton-restaurar");
+
+var botones = document.getElementsByTagName("button");
+var parrafos = document.getElementsByTagName("p");
+
+function ocultarP1(){
+    console.log(event);
+    parrafos[0].style.visibility = "hidden";
+    botones[0].removeEventListener("click",ocultarP1);
+    botones[0].addEventListener("click",mostrarP1);    
+}
+
+function mostrarP1(){
+    parrafos[0].style.visibility = "visible";
+    botones[0].removeEventListener("click",mostrarP1);
+    botones[0].addEventListener("click",ocultarP1);
+}
+
+function ocultarP2(){
+    console.log(event);
+    parrafos[1].style.visibility = "hidden";
+    botones[1].removeEventListener("click",ocultarP2);
+    botones[1].addEventListener("click",mostrarP2);    
+}
+
+function mostrarP2(){
+    parrafos[1].style.visibility = "visible";
+    botones[1].removeEventListener("click",mostrarP2);
+    botones[1].addEventListener("click",ocultarP2);
+}
+
+
+botones[0].addEventListener("click",ocultarP1);
+botones[1].addEventListener("click",ocultarP2);
